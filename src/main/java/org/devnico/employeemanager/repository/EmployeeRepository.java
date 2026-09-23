0d43f0b8a0140ea2f23e099e28cddee0d825e2c7
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-23 00:04:15 -0500

    EmployeeRepository ready

---
+++ src/main/java/org/devnico/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package org.devnico.employeemanager.repository;
+import org.devnico.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
