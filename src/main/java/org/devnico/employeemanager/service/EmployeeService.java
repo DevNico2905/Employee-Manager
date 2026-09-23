f5658d9ae03632ce38cc8afad778c915924ec413
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-23 00:05:30 -0500

    All services set

---
+++ src/main/java/org/devnico/employeemanager/service/EmployeeService.java
@@ -0,0 +1,39 @@
+package org.devnico.employeemanager.service;
+import org.devnico.employeemanager.model.Employee;
+import org.devnico.employeemanager.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee saveEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findById(Long id){
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee " + id + " not found!"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteById(id);
+    }
+}
