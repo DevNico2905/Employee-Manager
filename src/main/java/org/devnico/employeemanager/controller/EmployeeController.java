715418aeb0da4d38a67294fe05487d4b05a17a52
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-24 22:41:19 -0500

    All controllers done!

--- src/main/java/org/devnico/employeemanager/controller/EmployeeController.java
+++ src/main/java/org/devnico/employeemanager/controller/EmployeeController.java
@@ -3,11 +3,10 @@ package org.devnico.employeemanager.controller;
 import jakarta.validation.Valid;
 import org.devnico.employeemanager.model.Employee;
 import org.devnico.employeemanager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,10 +26,25 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeService.findAllEmployees());
     }
 
-    @GetMapping("find-by-id/{id}")
+    @GetMapping("/find-by-id/{id}")
     public ResponseEntity<Employee> findEmployeeById(@Valid
                                                      @PathVariable Long id){
         return ResponseEntity.ok(employeeService.findById(id));
     }
 
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.saveEmployee(employee));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        return ResponseEntity.ok(employeeService.updateEmployee(employee));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
 }
